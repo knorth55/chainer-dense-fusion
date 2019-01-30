d7b09a863fc0b25ff5f13f88f77af491af593ff6
Shingo Kitagawa <shingogo@hotmail.co.jp>
2019-01-30 15:30:41 +0900

    update visualize_dataset.py

--- examples/ycb/visualize_dataset.py
+++ examples/ycb/visualize_dataset.py
@@ -7,6 +7,7 @@ from chainer_dense_fusion.datasets.ycb import YCBVideoDataset
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--random', action='store_true')
+    parser.add_argument('--cad', action='store_true')
     args = parser.parse_args()
 
     dataset = YCBVideoDataset(split='val')
@@ -15,8 +16,10 @@ def main():
         random.shuffle(ids)
 
     for i in ids:
-        dataset.visualize(i)
-        dataset.visualize_3d(i)
+        if args.cad:
+            dataset.visualize_3d(i)
+        else:
+            dataset.visualize(i)
 
 
 if __name__ == '__main__':
