f7a256e823b9c9b1cd2b8a5c951a307f18ee97a8
Shingo Kitagawa <shingogo@hotmail.co.jp>
2019-03-24 14:27:23 +0900

    wip

--- chainer_dense_fusion/links/model/posenet_train_chain.py
+++ chainer_dense_fusion/links/model/posenet_train_chain.py
@@ -54,9 +54,28 @@ class PoseNetTrainChain(chainer.Chain):
                 cls_rot, cls_trans, cls_conf, h_img = self.posenet(
                     masked_img_var, masked_pcd_var, pcd_indice_var)
 
+                # (B, C, 4, N) -> (N, 4)
                 rot = cls_rot[0, bb_lbl]
                 rot = rot.transpose((1, 0))
                 rot = rot / np.linalg.norm(rot, axis=1)[:, None]
+                # (B, C, 3, N) -> (N, 3)
                 trans = cls_trans[0, bb_lbl]
                 trans = trans.transpose((1, 0))
+                # (B, C, N) -> (N, )
                 conf = cls_conf[0, bb_lbl]
+
+
+def _posenet_loss(rot, trans, conf):
+    base0 = 1 - 2 * (rot[:, 2]**2 + rot[:, 3]**2)
+    base1 = 2 * (rot[:, 1] * rot[:, 2] - rot[:, 0] * rot[:, 3])
+    base2 = 2 * (rot[:, 0] * rot[:, 2] + rot[:, 1] * rot[:, 3])
+    base3 = 2 * (rot[:, 1] * rot[:, 2] + rot[:, 3] * rot[:, 0])
+    base4 = 1 - 2 * (rot[:, 1]**2 + rot[:, 3]**2)
+    base5 = 2 * (rot[:, 2] * rot[:, 3] - rot[:, 0] * rot[:, 1])
+    base6 = 2 * (rot[:, 1] * rot[:, 3] - rot[:, 0] * rot[:, 2])
+    base7 = 2 * (rot[:, 0] * rot[:, 1] + rot[:, 2] * rot[:, 3])
+    base8 = 1 - 2 * (rot[:, 1]**2 + rot[:, 2]**2)
+    base = F.concat(
+        (base0, base1, base2, base3, base4, base5, base6, base7, base8),
+        axis=1)
+    base = base.reshape((len(base), 3, 3))
